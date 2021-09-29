563967b15519246024dcf7260535bcc05db7de9d
Dylan Erdini <erdini.dylan@gmail.com>
2021-09-28 21:43:51 -0300

    Agregago modificaciones

--- modelsdb/productos.js
+++ modelsdb/productos.js
@@ -4,8 +4,16 @@ const { sequelize } = require('../src/connection/sequelize')
 class Productos extends Model { }
 
 Productos.init({
-    direccion: {
+    nombre: {
         type: DataTypes.STRING,
-        
+        allowNull: false
+    },
+    precio: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }
-})
+}, {sequelize, modelName: 'Productos'})
+
+module.exports = {
+    Productos
+}
